54221711d55bebff4a0857ca8d36894dd5357d1d
Gerhard Görlich <gerhard@ggoerlich.at>
2016-11-17 21:10:59 +0100

    output to spreadsheet

--- parsecal.rb
+++ parsecal.rb
@@ -3,17 +3,31 @@ require 'rubygems' # Unless you install from the tarball or zip.
 require 'icalendar'
 require 'date'
 require 'open-uri'
+require 'axlsx'
 include Icalendar # Probably do this in your class to limit namespace overlap
 
 
-google_cal_url = 'TODO enter calender url here'
+google_cal_url = 'TODO Add your calendar URL here'
 total = 0
 min_by_month = Hash.new
 min_by_tag = Hash.new
 verbose = ARGV.index('-v')
+
+xls_package = Axlsx::Package.new
+wb = xls_package.workbook
+sheet = wb.add_worksheet(:name => "Timesheet")
+
+#time_style = xls_package.workbook.styles.add_style(:format_code => "yyyy-mm-dd", :border => Axlsx::STYLE_THIN_BORDER)
+
+
+time_style =	 sheet.styles.add_style(:format_code => "HH:MM", :border => Axlsx::STYLE_THIN_BORDER)
+
+
+sheet.add_row ["From", "To", "Duration", "Task"]
+
 open(google_cal_url) do |f|
 	# Parse Calendar
-	cals = Icalendar.parse(f)
+	cals = Icalendar::Calendar.parse(f)
 	# Parser returns an array of calendars because a single file
 	# can have multiple calendars.
 	cal = cals.first
@@ -24,8 +38,21 @@ open(google_cal_url) do |f|
 		#puts "#{event.dtstart.to_s} -  #{event.dtend.to_s}"
 		#puts "#{event.dtend.to_time - event.dtstart.to_time}"
 	 #	puts "#{event.duration}"
+
+	 	#sheet.add_row [event.dtstart.to_time, event.dtend.to_time, event.summary.to_s]
+
 		minutes = ((event.dtend.to_time - event.dtstart.to_time) / 60).to_i
 		summary = event.summary.to_s
+
+		# output to spreadsheet
+		rw = sheet.add_row
+		c_start = rw.add_cell(event.dtstart.to_time)
+		c_end = rw.add_cell(event.dtend.to_time)
+		c_diff = rw.add_cell("= #{c_end.r}-#{c_start.r}")
+		c_diff.style = time_style
+		c_sum = rw.add_cell(summary)
+
+
 		month_key = "#{event.dtstart.year}-#{event.dtstart.month}"
 		if min_by_month[month_key]
 			min_by_month[month_key] = min_by_month[month_key] + minutes
@@ -47,6 +74,8 @@ open(google_cal_url) do |f|
 	puts "========================"
 	puts "Total #{total/60}h #{total%60}min"
 end
+
+xls_package.serialize('timesheet.xlsx')
 
 puts "========================"
 min_by_month.each do |month,min|
